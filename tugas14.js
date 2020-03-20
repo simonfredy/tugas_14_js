a1011e5326afe9cf7cd1dec3706200aaaa2b1eab
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-20 19:46:36 +0700

    Tugas 14 JS

---
+++ tugas14.js
@@ -0,0 +1,13 @@
+var x = 37 / 4;
+
+// Hasil
+console.log('Hasil = ', x);
+
+//Round 
+console.log('Round = ', Math.round(x));
+
+//Ceil
+console.log('Ceil = ', Math.ceil(x));
+
+//Floor
+console.log('Floor = ', Math.floor(x));
